2bac9bea6157c9901e6fbd61597d0b7e3ba56fd1
B7-s <99633489+B7-s@users.noreply.github.com>
2024-03-27 09:49:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -84,7 +84,7 @@ module.exports = {
   KOYEB_API : process.env.KOYEB_API  || "false",
 
   REMOVE_BG_KEY : process.env.REMOVE_BG_KEY  || "",
-  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "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",
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "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",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
   HEROKU_APP_NAME:process.env.HEROKU_APP_NAME|| "",
   antilink_values:process.env.ANTILINK_VALUES|| "all",
